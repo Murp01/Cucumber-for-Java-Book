2f83a5669649e7e1354d36f66bb01ebfb750a98b
Murp01 <pmurphy35@hotmail.com>
2018-04-22 13:54:06 +0100

    Step Definition helper class added and implemented.
    The first two steps of requirement will now pass.  Comments have been
    added for a little guidance on helper class.
    
    You're boss you are.  Love you.

--- 05. Step Defs on the outside/ATM/src/test/java/nicebank/Steps.java
+++ 05. Step Defs on the outside/ATM/src/test/java/nicebank/Steps.java
@@ -15,6 +15,15 @@ import cucumber.api.Transform;
 import transforms.MoneyConverter;
 
 public class Steps {
+	knowsMyAccount helper;
+	
+	/*Step definition class constructor where an instance of knowsMyAccount
+	is created.  This means that the initialised object 'helper' can call
+	all methods from this class.  This instance 'should' keep the state between
+	different step definition calls */
+	public Steps() {
+		helper = new knowsMyAccount();
+	}
 
   //actor classes	
   class Account {
@@ -35,16 +44,16 @@ public class Steps {
 	  }
   }
   
-  //Helper Class -   
+  /*Helper Class named knowsMyaccount.  A method called getMyaccount
+  which will be called in the step def constructor */  
   class knowsMyAccount {
 	  private Account myAccount;
 	  
 	  public Account getMyAccount() {
 		  if (myAccount == null){
 			  myAccount = new Account();
-			  
-			  return myAccount;
 		  }
+		return myAccount;
 	  }
   }
 
@@ -53,16 +62,15 @@ public class Steps {
   
   @Given("^I have deposited \\$(\\d+\\.\\d+) in my account$")
   public void iHaveDeposited$InMyAccount(@Transform(MoneyConverter.class)Money amount) throws Throwable {
-      Account myAccount = new Account();
-	  myAccount.deposit(amount);
-	  
-	  Assert.assertEquals("Incorrect Account Balance - ", amount, myAccount.getBalance());
+      //Notice the helper instantiation calls two methods, in an order
+	  helper.getMyAccount().deposit(amount);	  
+	  Assert.assertEquals("Incorrect Account Balance - ", amount, helper.getMyAccount().getBalance());
   }
   
   @When("^I withdraw \\$(\\d+)$")
   public void iWithdraw$(int amount) throws Throwable {
 	  Teller teller = new Teller();
-	  teller.withdrawFrom(myAccount, amount);
+	  teller.withdrawFrom(helper.getMyAccount(), amount);
   }
 
   @Then("^\\$(\\d+) should be dispensed$")
